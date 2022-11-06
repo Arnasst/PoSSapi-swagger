85f4d092445f2619367d40acbc020b9561e1c369
Arnasst <59619111+Arnasst@users.noreply.github.com>
2022-11-06 15:37:22 +0200

    Added POST

--- PoSSapi/Controllers/ShiftController.cs
+++ PoSSapi/Controllers/ShiftController.cs
@@ -16,6 +16,13 @@ namespace PoSSapi.Controllers
             return Ok(Shift.GenerateRandom(id));
         }
 
+        // POST Shift
+        [HttpPost]
+        public ActionResult Post([FromBody] Shift shift)
+        {
+            return CreatedAtAction(nameof(Post), new { id = shift.Id }, shift);
+        }
+
         // PUT <ShiftController>/5
         [HttpPut("{id}")]
         public ActionResult Put(string id, [FromBody] Shift shift)
