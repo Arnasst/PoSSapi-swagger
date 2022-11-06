cbf278ef278683b1b2a2a40e45bda82878d4c1ba
Arnasst <59619111+Arnasst@users.noreply.github.com>
2022-11-06 15:03:08 +0200

    Added shift update, delete

---
+++ PoSSapi/Controllers/ShiftController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Classes;
+using Enums;
+
+namespace PoSSapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShiftController : ControllerBase
+    {
+        // PUT api/<ShiftController>/5
+        [HttpPut("{name}")]
+        public ActionResult Put(string id, [FromBody] Shift shift)
+        {
+            return Ok();
+        }
+
+        // DELETE api/<ShiftController>/5
+        [HttpDelete("{name}")]
+        public ActionResult Delete(string id)
+        {
+            return Ok();
+        }
+    }
+}
