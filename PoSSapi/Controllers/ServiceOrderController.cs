5ad5624fd124bf84632f6ac6e05d77e48e3fd82d
Viktorija Baikauskaitė <baikauskaitev@gmail.com>
2022-11-16 12:45:21 +0200

    Change property's OrderStatus location

--- PoSSapi/Controllers/ServiceOrderController.cs
+++ PoSSapi/Controllers/ServiceOrderController.cs
@@ -8,12 +8,12 @@ namespace PoSSapi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class ProductOrderController : GenericController<ProductOrder>
+public class ServiceOrderController : GenericController<Order>
 {
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [HttpGet]
-    public ActionResult GetAll([FromQuery] string? locationId, [FromQuery] int? status,
+    public ActionResult GetAll([FromQuery] string? locationId, [FromQuery] int? status, 
         [FromQuery] int itemsPerPage = 10, [FromQuery] int pageNum = 0)
     {
         if (itemsPerPage <= 0 || pageNum < 0)
@@ -26,10 +26,10 @@ public class ProductOrderController : GenericController<ProductOrder>
             return BadRequest("Invalid status");
         }
 
-        var objectList = new ProductOrder[itemsPerPage];
+        var objectList = new Order[itemsPerPage];
         for (var i = 0; i < itemsPerPage; i++)
         {
-            objectList[i] = RandomGenerator.GenerateRandom<ProductOrder>();
+            objectList[i] = RandomGenerator.GenerateRandom<Order>();
             
             if (status != null)
             {
@@ -39,24 +39,23 @@ public class ProductOrderController : GenericController<ProductOrder>
 
         return Ok(objectList);
     }
-    
+
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [HttpPatch("{id}")]
-    public ActionResult ChangeStatus(string id, [FromQuery][Required] int status)
+    public ActionResult ChangeStatus(string id, [FromQuery] [Required] int status)
     {
-        var productOrder = RandomGenerator.GenerateRandom<ProductOrder>(id);
-        
+        var productOrder = RandomGenerator.GenerateRandom<Order>(id);
+
         if (!Enum.IsDefined(typeof(OrderStatusState), status))
         {
             return BadRequest("Invalid status");
         }
-        
+
         var orderStatusState = (OrderStatusState)status;
         productOrder.OrderStatus = orderStatusState;
-        
+
         return Ok();
     }
-    
 }
