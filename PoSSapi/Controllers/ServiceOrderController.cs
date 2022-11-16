345ebac307ede0dd2513702fafac864f71ce2d23
Viktorija Baikauskaitė <baikauskaitev@gmail.com>
2022-11-16 19:34:45 +0200

    Fix issues

--- PoSSapi/Controllers/ServiceOrderController.cs
+++ PoSSapi/Controllers/ServiceOrderController.cs
@@ -13,28 +13,19 @@ public class ServiceOrderController : GenericController<Order>
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [HttpGet]
-    public ActionResult GetAll([FromQuery] string? locationId, [FromQuery] int? status, 
+    public ActionResult GetAll([FromQuery] string? locationId, [FromQuery] OrderStatusState? status, 
         [FromQuery] int itemsPerPage = 10, [FromQuery] int pageNum = 0)
     {
         if (itemsPerPage <= 0 || pageNum < 0)
         {
             return BadRequest("Invalid itemsPerPage or pageNum");
         }
-        
-        if (status != null && !Enum.IsDefined(typeof(OrderStatusState), status))
-        {
-            return BadRequest("Invalid status");
-        }
 
         var objectList = new Order[itemsPerPage];
         for (var i = 0; i < itemsPerPage; i++)
         {
             objectList[i] = RandomGenerator.GenerateRandom<Order>();
-            
-            if (status != null)
-            {
-                objectList[i].OrderStatus = (OrderStatusState) status;
-            }
+            objectList[i].OrderStatus = status ?? OrderStatusState.New;
         }
 
         return Ok(objectList);
@@ -44,17 +35,10 @@ public class ServiceOrderController : GenericController<Order>
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [HttpPatch("{id}")]
-    public ActionResult ChangeStatus(string id, [FromQuery] [Required] int status)
+    public ActionResult ChangeStatus(string id, [FromQuery] [Required] OrderStatusState? status)
     {
         var productOrder = RandomGenerator.GenerateRandom<Order>(id);
-
-        if (!Enum.IsDefined(typeof(OrderStatusState), status))
-        {
-            return BadRequest("Invalid status");
-        }
-
-        var orderStatusState = (OrderStatusState)status;
-        productOrder.OrderStatus = orderStatusState;
+        productOrder.OrderStatus = status ?? OrderStatusState.New;
 
         return Ok();
     }
