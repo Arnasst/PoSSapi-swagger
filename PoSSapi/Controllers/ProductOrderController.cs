67cfc26867a5ac7d81b98e3fbb28aed2b88b5711
Viktorija Baikauskaitė <baikauskaitev@gmail.com>
2022-11-16 12:25:07 +0200

    Add new controllers

--- PoSSapi/Controllers/ProductOrderController.cs
+++ PoSSapi/Controllers/ProductOrderController.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Classes;
 using Enums;
 using Microsoft.AspNetCore.Mvc;
@@ -10,13 +11,34 @@ namespace PoSSapi.Controllers;
 public class ProductOrderController : GenericController<ProductOrder>
 {
     [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [HttpGet]
+    public ActionResult GetAll([FromQuery] string? locationId, [FromQuery] int itemsPerPage=10,
+        [FromQuery] int pageNum=0)
+    {
+        if (itemsPerPage <= 0 || pageNum < 0)
+        {
+            return BadRequest("Invalid itemsPerPage or pageNum");
+        }
+
+        var objectList = new ProductOrder[itemsPerPage];
+        for (var i = 0; i < itemsPerPage; i++)
+        {
+            objectList[i] = RandomGenerator.GenerateRandom<ProductOrder>();
+        }
+        
+        return Ok(objectList);
+    }
+    
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [HttpPatch("{id}")]
-    public ActionResult ChangeStatus(string id, int status)
+    public ActionResult ChangeStatus(string id, [FromQuery][Required] int status)
     {
-        var productOrder = RandomGenerator.GenerateRandom<ProductOrder>();
+        var productOrder = RandomGenerator.GenerateRandom<ProductOrder>(id);
         
-        if (Enum.IsDefined(typeof(OrderStatusState), status))
+        if (!Enum.IsDefined(typeof(OrderStatusState), status))
         {
             return BadRequest("Invalid status");
         }
