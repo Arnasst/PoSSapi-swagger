5b2c5c9bd3ec644ab44a79c5cb09efdcb09c83e0
Arnasst <59619111+Arnasst@users.noreply.github.com>
2022-11-18 18:03:37 +0200

    Fixed mistakes

--- PoSSapi/Controllers/PaymentController.cs
+++ PoSSapi/Controllers/PaymentController.cs
@@ -63,5 +63,17 @@ public class PaymentController : GenericController<Payment>
     {
         return Ok();
     }
+
+    /** <summary>Emails a specific invoice to a specific customer</summary>
+    * <param name="id" example="">Id of the payment that you want to send the invoice of</param>
+    * <param name="customerId" example="">Id of the customer whose recieving the invoice</param>
+    */
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [HttpPost("{id}/invoice/{customerId}")]
+    public ActionResult SendInvoiceToCustomer(string id, string customerId)
+    {
+        return Ok();
+    }
 }
 
